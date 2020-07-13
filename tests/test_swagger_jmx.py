eac94f4784464e733f82e5ebdc7345cb240beb56
lijiawei <1456470136@qq.com>
2020-07-13 16:56:00 +0800

    Update test_swagger_jmx.py

--- tests/test_swagger_jmx.py
+++ tests/test_swagger_jmx.py
@@ -4,6 +4,6 @@ from swaggerjmx.convert import conversion
 from swaggerjmx.settings import Settings as ST
 
 # ST.swagger_url = 'http://10.0.0.0:6003/v2/api-docs'
-ST.swagger_url_json_path = 'test.json'
+ST.swagger_url_json_path = './test.json'
 ST.report_path = 'jmx'
 conversion()
