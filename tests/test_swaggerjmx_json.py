36ec40ef3cae4cf5117c9688b7dbfcb37dd4f528
Lijiawei <1456470136@qq.com>
2022-06-05 22:44:57 +0800

    test: add swagger test data

--- tests/test_swaggerjmx_json.py
+++ tests/test_swaggerjmx_json.py
@@ -3,7 +3,7 @@ from swaggerjmx.convert import conversion
 from swaggerjmx.settings import Settings as ST
 
 # swagger or yapi json
-ST.swagger_url_json_path = 'test.json'
+ST.swagger_url_json_path = './data/swagger.json'
 #  report_path
 ST.report_path = 'jmx'
 # 开始转换
